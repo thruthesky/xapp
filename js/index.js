91eb0d9ed52ed5231515ac923b685ae54e9a41bb
jeraisy21 <thruthesky@gmail.com>
2016-07-15 14:31:05 +0900

    work on xapp

--- js/index.js
+++ js/index.js
@@ -1,26 +1,40 @@
+/**
+ * @file index.js
+ */
+
+/**
+ *      D E F A L U L T  Variables
+ */
+
+var url_wordpress= 'http://wordpress46b1.org/index.php';
 
 
+
+
+
+
+/**
+ *      START SCRIPT
+ */
 $(function(){
-    var url = 'http://work.org/tmp/4-secs.php';
-    console.log('going to load:  ' + url);
-    var count = 1;
-    setInterval(function(){
-        //console.log(count++);
-    }, 1000);
 
-    //console.log( db.getCache( 'test4' ) );
+    /*
+     xapp.wp_query({
+     'slug' : 'its',
+     'posts_per_page': 3
+     });
+     */
 
-
-
-    xapp.cache( {
-        'url' : url,
-        'id' : 'test4',
-        'expire' : 60 * 60,
+    xapp.wp_get_categories({
+        'expire' : 86400 * 7,
         'success' : function( re ) {
             console.log(re);
-            $('#main').prepend( re );
         },
-        'failure' : function( re ) { console.log('failure', re); }
-    } );
+        'failure' : function( re ) {
+            alert('ERROR on getting categories');
+        }
+    });
 
+
+    console.log('index.js $(function() { ... }); finished.')
 });
